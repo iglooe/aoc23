d6cccd219cfc6db319047ed40cbb8137dba044e3
iglooe <day.derek7@protonmail.com>
2023-12-07 14:22:19 -0600

    day 2 part 1

--- puzzles/day-2/day-2-a.ts
+++ puzzles/day-2/day-2-a.ts
@@ -1,9 +1,34 @@
 import { readData } from '../../shared.ts';
 import chalk from 'chalk';
 
+const maxCount = {
+  red: 12,
+  green: 13,
+  blue: 14,
+};
+
 export async function day2a(dataPath?: string) {
   const data = await readData(dataPath);
-  return 0;
+  console.log(data);
+
+  return data
+    .map((line) => {
+      return line
+        .split(': ')[1]
+        .split(';')
+        .map((set) => {
+          const pulls = set.trim().split(', ');
+          console.log(pulls);
+          return pulls.every((pull) => {
+            const [count, color] = pull.split(' ');
+            return maxCount[color] >= Number(count);
+          });
+        })
+        .every((p) => p);
+    })
+    .reduce((s, result, i) => {
+      return result ? s + (i + 1) : s;
+    }, 0);
 }
 
 const answer = await day2a();
